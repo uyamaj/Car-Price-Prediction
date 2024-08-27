a4f69e2955d57bd13d8bea68e02703ae6fda4d70
Jane Frances <r.merry944@gmail.com>
2024-08-27 10:48:14 +0100

    new file

--- application.py.py
+++ application.py.py
@@ -4,11 +4,9 @@ import requests
 import pickle
 import numpy as np
 import sklearn
-import joblib
 from sklearn.preprocessing import StandardScaler
 app = Flask(__name__)
-model = joblib.load('car_price_predict.model.pkl')
-# model = pickle.load(open('car_price_prediction.model.pkl', 'rb'))
+model = pickle.load(open('car_price_prediction.model.pkl', 'rb'))
 @app.route('/',methods=['GET'])
 def Home():
     return render_template('index.html')
